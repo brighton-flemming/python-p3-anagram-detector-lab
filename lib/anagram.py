492ea1d978e64ede8d26a0a82b4c3eaf28144dcc
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-08-25 00:41:27 +0300

    Mission Passed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,3 +1,12 @@
 class Anagram:
-    def __init__(self) -> None:
+    def __init__(self, word):
+        self._word = word
+    
+    def match(self, word_list):
+        anagrams = []
+        for candidate in word_list:
+            if sorted(candidate.lower()) == sorted(self._word.lower()) and candidate.lower() != self._word:
+             anagrams.append(candidate)
+
+        return anagrams
         pass
